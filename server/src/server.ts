c488147c896c452db4fe285f501b45e137099d4b
vanessabach <vanessabach.r@gmail.com>
2022-09-14 17:29:58 -0300

    starting database

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,7 +2,22 @@ import express from 'express';
 
 const app = express()
 
-app.get('/ads', (request, response) => {
+// HTTP methods/API RESTFUL
+//HTTP Status Codes
+// 200 Sucesso 
+// 300 Redirecionamento 
+// 400 Algum erro da nossa App 
+//500 Erros inesperados
+
+app.get('/games', (request, response) => {
+  return response.json([]);
+});
+
+app.post('/ads', (request, response) => {
+  return response.status(201).json([]);
+});
+
+app.get('/games/id/ads', (request, response) => {
   return response.json([
       { id: 1, name: 'Anúncio 1' },
       { id: 2, name: 'Anúncio 2' },
@@ -11,4 +26,9 @@ app.get('/ads', (request, response) => {
       { id: 5, name: 'Anúncio 5' },
   ]);
 });
+
+app.get('/ads/:id/discord', (request, response) => {
+  return response.json([]);
+});
+
 app.listen(3333);
